9106b3348578833f85fef71957a8ce3af260963a
Haabiy <abiy.dema@gmail.com>
2024-05-27 22:17:35 +0200

    Testing for deployment

---
+++ dist/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('./src/css/style.css').Config} */
+module.exports = {
+  content: ['./dist/*.{html,js}'],
+  theme: {
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      fontFamily : {
+        sans: ['Josefin Sans', 'sans-serif'],
+        alata : ['Alata', 'sans-serif'],
+      }
+    },
+    width:{
+      'imagex': '80rem',
+    },
+    height:{
+      'hero': '25rem',
+    },
+  },
+  plugins: [],
+}
+
